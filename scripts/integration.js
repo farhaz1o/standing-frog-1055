d25d88f3bd19d9dd06db88b7d322de51982d1aaa
GauravSingh72 <1gaurav2singh@gmail.com>
2022-09-29 13:46:10 +0530

    Features page HTML & CSS done

---
+++ scripts/integration.js
@@ -0,0 +1,309 @@
+let data = [
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/asana.png?width=200nname=asana.png",
+    t: "Asana",
+    p: "Track time right from your Asana tasks without switching to Harvest.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/basecamp.png?width=200nname=basecamp.png",
+    t: "Basecamp",
+    p: "Track time from your to-dos with a Harvest browser extension.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/github.png?width=200nname=github.png",
+    t: "GitHub",
+    p: "Track time from issues and pull requests with a Harvest browser extension.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/slack.png?width=200nname=slack.png",
+    t: "Slack",
+    p: "Make time tracking part of your team’s conversation",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/trello.png?width=200nname=trello.png",
+    t: "Trello",
+    p: "Track time from cards with the Harvest Power-Up or browser extension.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/qbo.png?width=300nname=qbo.png",
+    t: "QuickBooks Online",
+    p: "Seamlessly copy Harvest invoices to QuickBooks Online",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/xero.png?width=200nname=xero.png",
+    t: "Xero",
+    p: "Automatically copy Harvest invoices to Xero",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/stripe.png?width=300nname=stripe.png",
+    t: "Stripe",
+    p: "Accept online payments from your clients",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/paypal.png?width=300nname=paypal.png",
+    t: "PayPal",
+    p: "Accept online payments from your clients",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/chrome.png?width=200nname=chrome.png",
+    t: "Chrome",
+    p: "Track time to Harvest from the Chrome toolbar",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/jira.png?width=300nname=jira.png",
+    t: "Jira",
+    p: "Track time to Harvest without leaving Jira",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/outlook.png?width=200nname=outlook.png",
+    t: "Outlook.com",
+    p: "Pull your Outlook calendar events into your Harvest timesheet.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/google-calendar.png?width=200nname=google-calendar.png",
+    t: "Google Calendar",
+    p: "Pull your Google Calendar events into your Harvest timesheet.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/zapier.png?width=200nname=zapier.png",
+    t: "Zapier",
+    p: "Connect Harvest to 1,000+ apps with automated workflows using Zapier.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/ifttt.png?width=200nname=ifttt.png",
+    t: "IFTTT",
+    p: "Create simple connections between Harvest and the apps you use every day.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/zendesk.png?width=200nname=zendesk.png",
+    t: "Zendesk",
+    p: "Track time to Harvest without leaving Zendesk.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/alfred.png?width=200nname=alfred.png",
+    t: "Alfred",
+    p: "Control your Harvest timers using keyboard shortcuts with this Alfred workflow.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/workspace.png?width=200nname=workspace.png",
+    t: "Google Workspace",
+    p: "Sign in to Harvest faster with your Google credentials",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/teamwork.png?width=300nname=teamwork.png",
+    t: "Teamwork Projects",
+    p: "Track time to Harvest from Teamwork Projects.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/flow.png?width=300nname=flow.png",
+    t: "Flow",
+    p: "Time track in Harvest without leaving Flow.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/apollo-icon@2x.png?width=300nname=apollo-icon@2x.png",
+    t: "Apollo",
+    p: "Create Harvest invoices from your Apollo time entries.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/firefox.png?width=200nname=firefox.png",
+    t: "Firefox",
+    p: "Track time to Harvest from the Firefox toolbar",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/edge.png?width=300nname=edge.png",
+    t: "Edge",
+    p: "Track time to Harvest from the Edge toolbar",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/akita-icon@2x.png?width=200nname=akita-icon@2x.png",
+    t: "Akita",
+    p: "Deliver customer success with Akita and Harvest.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/beanstalk-icon@2x.png?width=200nname=beanstalk-icon@2x.png",
+    t: "Beanstalk",
+    p: "Log your time as you code.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/bidsketch-icon@2x.png?width=200nname=bidsketch-icon@2x.png",
+    t: "Bidsketch",
+    p: "Turn your proposals into invoices.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/breeze-icon@2x.png?width=200nname=breeze-icon@2x.png",
+    t: "Breeze",
+    p: "Track time to Harvest without leaving Breeze.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/brightpod-icon@2x.png?width=200nname=brightpod-icon@2x.png",
+    t: "Brightpod",
+    p: "Track time to Harvest within Brightpod.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/clickup-icon@2x.png?width=200nname=clickup-icon@2x.png",
+    t: "ClickUp",
+    p: "Track time on ClickUp tasks with Harvest.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/donedone-icon@2x.png?width=200nname=donedone-icon@2x.png",
+    t: "DoneDone",
+    p: "Track time in Harvest as you track your work.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/formstack.png?width=300nname=formstack.png",
+    t: "Formstack",
+    p: "Generate custom invoices and letters with Harvest data.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/freshservice.png?width=200nname=freshservice.png",
+    t: "Freshservice",
+    p: "Track time directly from your Freshservice tickets.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/invoice-payout-icon@2x.png?width=200nname=invoice-payout-icon@2x.png",
+    t: "InvoicePayout",
+    p: "Calculate sales commissions from your Harvest invoices.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/parakeeto.png?width=200nname=parakeeto.png",
+    t: "Parakeeto",
+    p: "Measure your agency’s performance n profitability.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/databox-icon@2x.png?width=200nname=databox-icon@2x.png",
+    t: "Databox",
+    p: "Include your Harvest data in your Databox dashboards.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/price-cost-icon@2x.png?width=200nname=price-cost-icon@2x.png",
+    t: "PricenCost",
+    p: "Track costs and increase project profitability",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/herobi-icon@2x.png?width=200nname=herobi-icon@2x.png",
+    t: "heroBI",
+    p: "A performance tracker for your agency",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/klipfolio-icon@2x.png?width=200nname=klipfolio-icon@2x.png",
+    t: "Klipfolio",
+    p: "Include your Harvest data in your Klipfolio dashboards.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/stitch-icon@2x.png?width=200nname=stitch-icon@2x.png",
+    t: "Stitch",
+    p: "Get your Harvest data into your data warehouse.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/fuss.png?width=200nname=fuss.png",
+    t: "Fuss",
+    p: "Track time in Harvest directly from Fuss.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/rindle.png?width=200nname=rindle.png",
+    t: "Rindle",
+    p: "Project automation with seamless Harvest time tracking.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/eazybi-icon@2x.png?width=200nname=eazybi-icon@2x.png",
+    t: "eazyBI",
+    p: "Custom timesheet and invoice reports n dashboards.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/projectt.png?width=200nname=projectt.png",
+    t: "Projectt",
+    p: "Visualize your Harvest data in Projectt.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/time-connect-icon@2x.png?width=200nname=time-connect-icon@2x.png",
+    t: "Time Connect",
+    p: "Sync your Harvest data with Microsoft Dynamics.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/harvestsync-icon@2x.png?width=200nname=harvestsync-icon@2x.png",
+    t: "HarvestSync",
+    p: "Send your Harvest time entries to Salesforce.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/harvey-icon@2x.png?width=300nname=harvey-icon@2x.png",
+    t: "Harvey",
+    p: "Bring Harvest time tracking reports into Slack.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/plecto-icon@2x-1.png?width=200nname=plecto-icon@2x-1.png",
+    t: "Plecto",
+    p: "Create real-time dashboards with your Harvest data.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/zendesk-sell.png?width=300nname=zendesk-sell.png",
+    t: "Zendesk Sell",
+    p: "Track time right from leads and deals in Zendesk.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/linux-timer-icon@2x.png?width=300nname=linux-timer-icon@2x.png",
+    t: "Linux Timer",
+    p: "Track time to Harvest from your Linux desktop.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/slack-reminder-icon@2x.png?width=300nname=slack-reminder-icon@2x.png",
+    t: "Slack Reminder",
+    p: "Remind your team to track and submit time in Slack.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/python-library-icon@2x.png?width=300nname=python-library-icon@2x.png",
+    t: "Python Library",
+    p: "Create Python applications with the Harvest API.",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/integrately-icon@2x.png?width=300nname=integrately-icon@2x.png",
+    t: "Integrately",
+    p: "Integrate Harvest with 100’s of apps in 1 click",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/tugger-icon@2x-1.png?width=300nname=tugger-icon@2x-1.png",
+    t: "Tugger",
+    p: "Easily get Harvest data into analytics tools",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/zwapgrid-icon@2x.png?width=300nname=zwapgrid-icon@2x.png",
+    t: "Zwapgrid",
+    p: "Seamless integration between Harvest and Fortnox",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/coupler-logo.png?width=300nname=coupler-logo.png",
+    t: "Coupler.io",
+    p: "Export data to Google Sheets, Excel, and BigQuery",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations/gosimplo.png?width=300nname=gosimplo.png",
+    t: "GoSimplo",
+    p: "Get business intelligence in five minutes",
+  },
+  {
+    i: "https://www.getharvest.com/hs-fs/hubfs/integrations-new/NiftyN512x.png?width=300nname=NiftyN512x.png",
+    t: "Nifty",
+    p: "Track time in Harvest from within Nifty.",
+  },
+  // {
+  //     i:"",
+  //     t:"",
+  //     p:"",
+  // }
+];
+
+let appendData = () => {
+  let cont = document.querySelector("#container");
+  cont.innerHTML = null;
+  data.forEach(({ i, t, p }) => {
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = i;
+    let tit = document.createElement("h3");
+    tit.innerText = t;
+    let par = document.createElement("p");
+    par.innerText = p;
+    div.append(img, tit, par);
+    cont.append(div);
+  });
+};
+appendData();
