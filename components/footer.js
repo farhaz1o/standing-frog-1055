fd3d610da3af02d33c7d501bf0f0a18057c2f951
faraz1o <fk7ansari@gmail.com>
2022-10-02 02:07:37 +0530

    added

---
+++ components/footer.js
@@ -0,0 +1,104 @@
+function footer() {
+    return`
+    <div id="footbox1">
+        <div id="footlogo">
+            <a href="">
+            <img src="https://logos-world.net/wp-content/uploads/2022/04/Harvest-Logo.png">
+            </a>
+        </div>
+        <div id="footh">
+            <p class="foothh">Harvest</p>
+            <a href="">
+            <p class="footp">Home</p>
+        </a>
+            <a href="">
+            <p class="footp">Why harvest</p>
+        </a>
+            <a href="">
+            <p class="footp">Features</p>
+        </a>
+            <a href="">
+            <p class="footp">Pricing</p>
+        </a>
+            <a href="">
+            <p class="footp">Integrations</p>
+        </a>
+            <a href="">
+            <p class="footp">Apps</p>
+        </a>
+            <a href="">
+            <p class="footp">Security</p>
+        </a>
+        </div>
+        <div id="footc">
+            <p class="foothh">Community</p>
+            <a href="">
+            <p class="footp">Customer stories</p>
+        </a>
+            <a href="">
+            <p class="footp">Resources </p>
+        </a>
+            <a href="">
+            <p class="footp">Webinars</p>
+        </a>
+            <a href="">
+            <p class="footp">Help & support</p>
+        </a>
+            <a href="">
+            <p class="footp">Integrate with Harvest</p>
+        </a>
+            <a href="">
+            <p class="footp">Time Well Spent</p>
+        </a>
+            <a href="">
+            <p class="footp">Time in This Time</p>
+        </a>
+        </div>
+        <div id="footcomp">
+            <p class="foothh">Company</p>
+            <a href="">
+            <p class="footp">About us</p>
+        </a>
+            <a href="">
+            <p class="footp">Blog </p>
+        </a>
+            <a href="">
+            <p class="footp">Careers</p>
+        </a>
+            <a href="">
+            <p class="footp">Legal</p>
+        </a>
+            <a href="">
+            <p class="footp">Contact us</p>
+        </a>
+          
+        </div>
+
+    </div>
+    
+    <div id="forecast">
+        <p>We also make: Harvest Forecast – the fast and simple way to schedule your team across projects.</p>
+
+    </div>
+    <div id="footbox2">
+    <div id="two">
+        <p>© 2006-2022 Harvest</p>
+    </div>
+    <div id="social">
+        <div class="soc">
+            <img class="socaili" src="https://www.freepnglogos.com/uploads/twitter-logo-png/black-and-white-twitter-logo-png-hq-download-25.png">
+            <p>Twitter</p>
+        </div>
+        <div class="soc">
+            <img class="socaili" src="https://cdn-icons-png.flaticon.com/512/408/408805.png?w=360">
+            <p>LinkedIn</p>
+        </div>
+        <div class="soc">
+            <img class="socaili" src="https://www.pngkey.com/png/full/674-6745653_instagram-icon.png">
+            <p>Instagram</p>
+        </div>
+        
+    </div>
+</div>`
+}
+export default footer;
